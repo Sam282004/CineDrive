105309b90d093575045d6619e43c9b2c80c11980
Samuel Llaccho <89850223+Sam282004@users.noreply.github.com>
2023-09-14 15:39:36 -0500

    Creacion de CineDrive

---
+++ maincinedrive/src/main/java/com/example/maincinedrive/MaincinedriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.maincinedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaincinedriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaincinedriveApplication.class, args);
+	}
+
+}
---
+++ maincinedrive/src/test/java/com/example/maincinedrive/MaincinedriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maincinedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaincinedriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
